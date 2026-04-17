83ae1a5c675bf36dc6c1b63aa03fc50ceb70bb02
Mehak-2005 <mehakm9620@gmail.com>
2026-04-17 20:10:23 +0530

    Delete Recommendation-System directory

--- Recommendation-System/tests/test_engine.py
+++
@@ -1,9 +0,0 @@
-from engine.orchestrator import RecommendationOrchestrator
-from data.repositories import ContentRepository, InteractionRepository
-
-def test_engine():
-    engine = RecommendationOrchestrator(ContentRepository(), InteractionRepository())
-    recs = engine.get_recommendations("u1")
-    assert isinstance(recs, list)
-
-    
